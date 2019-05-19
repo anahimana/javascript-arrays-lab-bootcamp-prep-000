f3713ab7e912113a99e01f9f12b2ac6348f9204b
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 05:10:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(array) {
-  return kittens
+  return kittens;
 }
 
 console.log(destructivelyAppendKitten(kittens));
