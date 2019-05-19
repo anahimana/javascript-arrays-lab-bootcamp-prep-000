79b32dc8dd8be5572c5ae51e1283e756a25b19a7
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 05:15:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ function destructivelyAppendKitten(name) {
   return kittens;
 }
 
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
 // console.log(destructivelyAppendKitten(kittens));
 // console.log(kittens);
